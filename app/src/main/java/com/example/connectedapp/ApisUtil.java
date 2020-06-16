0a5012ce50259e0102e54d6cf21222400f3c8753
Elly Wenani <ellywenani@gmail.com>
2020-06-16 23:07:30 +0300

    Created a class ApisUtil and added some code

--- app/src/main/java/com/example/connectedapp/ApisUtil.java
+++ app/src/main/java/com/example/connectedapp/ApisUtil.java
@@ -37,7 +37,7 @@ public class ApisUtil {
             scanner.useDelimiter("\\A");
 
             boolean hasData = scanner.hasNext();
-            
+
             if (hasData) {
                 return scanner.next();
             } else {
